371d29f07a19c2b3aadc99fa260c9323c3539636
James Halliday <mail@substack.net>
2012-12-25 02:17:36 -0800

    preview box with mouse{over,out} freeze/thaw

--- browser/preview.js
+++ browser/preview.js
@@ -5,13 +5,38 @@ module.exports = Preview;
 
 function Preview (target) {
     if (!(this instanceof Preview)) return new Preview(target);
-    this.target = target;
+    var self = this;
+    self.target = target;
+    self.length = 0;
+    
+    var iv = setInterval(interval, 5000);
+    var ix = 0;
+    function interval () {
+        var prev = target.querySelector('.preview.active');
+        if (prev) prev.className = 'preview';
+        
+        var div = target.childNodes[ix++ % self.length];
+        div.className = 'preview active';
+    }
+    
+    target.addEventListener('mouseover', function () {
+        clearInterval(iv);
+        iv = null;
+    });
+    
+    target.addEventListener('mouseout', function () {
+        if (!iv) iv = setInterval(interval, 5000);
+    });
 }
 
 Preview.prototype.push = function (doc) {
+    this.length ++;
     var div = hyperglue(html, {
         '.title' : doc.title,
         '.image' : 'xxx'
     });
+    if (this.length === 1) {
+        div.className = 'preview active';
+    }
     this.target.appendChild(div);
 };
