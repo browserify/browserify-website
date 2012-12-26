383fce04170798c4668dd12d3ea52e9a6ef3a3a0
James Halliday <mail@substack.net>
2012-12-25 23:45:01 -0800

    [].join instead of +s

--- browser/html/preview.js
+++ browser/html/preview.js
@@ -1,12 +1,17 @@
-module.exports
-= '<div class="preview">'
-  + '<div class="label">recent articles</div>'
-  + '<div class="image"></div>'
-  + '<div class="title">'
-    + '<a></a>'
-  + '</div>'
-  + '<div class="arrows">'
-    + '<div class="left arrow"></div>'
-    + '<div class="right arrow"></div>'
-  + '</div>'
-+ '</div>'
+module.exports = [
+  '<div class="preview">',
+    '<div class="label">',
+      'recent articles',
+    '</div>',
+    '<div class="inner">',
+      '<div class="image"></div>',
+      '<div class="title">',
+        '<a></a>',
+      '</div>',
+      '<div class="arrows">',
+        '<div class="left arrow"></div>',
+        '<div class="right arrow"></div>',
+      '</div>',
+    '</div>',
+  '</div>'
+].join('\n');
