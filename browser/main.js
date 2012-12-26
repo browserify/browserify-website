155e82d6fcd815686805e21f219cc5adf0212287
James Halliday <mail@substack.net>
2012-12-26 04:03:45 -0800

    path routing for the articles

--- browser/main.js
+++ browser/main.js
@@ -1,8 +1,15 @@
 var JSONStream = require('JSONStream');
 var http = require('http');
 
-var articles = require('./article')(document.querySelector('#articles'));
-var previews = require('./preview')(document.querySelector('#previews'));
+var divs = {
+    splash : document.querySelector('#splash'),
+    articles : document.querySelector('#articles'),
+    articleBox : document.querySelector('#article-box'),
+    previews : document.querySelector('#previews')
+};
+
+var articles = require('./article')(divs.articles);
+var previews = require('./preview')(divs.previews);
 
 http.get({ path : '/blog.json?inline=html' }, function (res) {
     var parser = JSONStream.parse([ true ]);
@@ -18,3 +25,15 @@ var cloneCode = document.querySelector('#clone-me .code');
 cloneCode.textContent = cloneCode.textContent
     .replace(/\$REMOTE/, 'http://' + window.location.host + '/blog.git')
 ;
+
+if (window.location.pathname === '/articles') {
+    hide(divs.splash);
+    show(divs.articleBox);
+}
+else {
+    show(divs.splash);
+    hide(divs.articleBox);
+}
+
+function hide (e) { e.style.display = 'none' }
+function show (e) { e.style.display = 'block' }
