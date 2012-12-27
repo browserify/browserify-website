e98b903aafa1f7ad0e1d47c66e0de9b7370ca578
James Halliday <mail@substack.net>
2012-12-27 12:38:46 -0800

    early return

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ var glog = require('glog')(process.argv[3]);
 var ecstatic = require('ecstatic')(__dirname + '/static');
 
 var server = http.createServer(function (req, res) {
-    if (glog.test(req.url)) {
-        glog(req, res);
-    }
-    else if (/^\/[^\.\/]+$/.test(req.url)) {
+    if (glog.test(req.url)) return glog(req, res);
+    
+    if (/^\/[^\.\/]+$/.test(req.url)) {
         req.url = '/';
-        ecstatic(req, res);
     }
-    else ecstatic(req, res);
+    ecstatic(req, res);
 });
 server.listen(Number(process.argv[2]));
