7aeb5870f1a3105c215115e7df037036c79b2e82
James Halliday <mail@substack.net>
2012-12-26 16:31:15 -0800

    use [].join() instead of + for html

--- browser/html/article.js
+++ browser/html/article.js
@@ -1,23 +1,24 @@
-module.exports
-= '<div class="article">'
-  + '<div class="title">'
-    + '<a name="name"></a>'
-  + '</div>'
-  + '<div class="inner">'
-    + '<div class="headers">'
-      + '<div class="header">'
-        + '<div class="key">commit</div>'
-        + '<div class="value commit"></div>'
-      + '</div>'
-      + '<div class="header">'
-        + '<div class="key">Author:</div>'
-        + '<div class="value author"></div>'
-      + '</div>'
-      + '<div class="header">'
-        + '<div class="key">Date:</div>'
-        + '<div class="value date"></div>'
-      + '</div>'
-    + '</div>'
-    + '<div class="body"></div>'
-  + '</div>'
-+ '</div>'
+module.exports = [
+  '<div class="article">',
+    '<div class="title">',
+      '<a name="name"></a>',
+    '</div>',
+    '<div class="inner">',
+      '<div class="headers">',
+        '<div class="header">',
+          '<div class="key">commit</div>',
+          '<div class="value commit"></div>',
+        '</div>',
+        '<div class="header">',
+          '<div class="key">Author:</div>',
+          '<div class="value author"></div>',
+        '</div>',
+        '<div class="header">',
+          '<div class="key">Date:</div>',
+          '<div class="value date"></div>',
+        '</div>',
+      '</div>',
+      '<div class="body"></div>',
+    '</div>',
+  '</div>'
+].join('\n')
