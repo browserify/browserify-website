3e7678de776da5bfc6e5766ecb219d349a0d3791
James Halliday <mail@substack.net>
2012-12-27 21:40:42 -0800

    streaming json search

--- bin/search.js
+++ bin/search.js
@@ -1,2 +1,6 @@
+var JSONStream = require('JSONStream');
+var stringify = JSONStream.stringify();
+stringify.pipe(process.stdout);
+
 var search = require('../lib/search');
-search(process.argv.slice(2).join(' '));
+search(process.argv.slice(2).join(' ')).pipe(stringify);
