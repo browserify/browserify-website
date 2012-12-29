8e8738e3086b2fdb38596864a6d3f3d01d9e2c52
James Halliday <mail@substack.net>
2012-12-28 16:48:14 -0800

    /search.json route

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ var http = require('http');
 var glog = require('glog')(process.argv[3]);
 var ecstatic = require('ecstatic')(__dirname + '/static');
 
+var search = require('./lib/search');
+var qs = require('querystring');
+
 var server = http.createServer(function (req, res) {
     if (glog.test(req.url)) return glog(req, res);
+    
+    if (req.url.split('?')[0] === '/search.json') {
+        var params = qs.parse(req.url.split('?')[1]);
+        
+        search(params.query, function (err, results) {
+            if (err) {
+                res.statusCode = 400;
+                res.end(err);
+            }
+            else {
+                res.setHeader('content-type', 'application/json');
+                res.end(JSON.stringify(results));
+            }
+        });
+        return;
+    }
     
     if (/^\/[^\.\/]+$/.test(req.url)) {
         req.url = '/';
