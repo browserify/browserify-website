2c6664a30c355ed67cfc702d97ffb1603a65dc08
James Halliday <mail@substack.net>
2012-12-30 20:38:26 -0800

    bin/ files not needed

--- bin/search.js
+++
@@ -1,6 +0,0 @@
-var JSONStream = require('JSONStream');
-var stringify = JSONStream.stringify();
-stringify.pipe(process.stdout);
-
-var search = require('../lib/search');
-search(process.argv.slice(2).join(' ')).pipe(stringify);
